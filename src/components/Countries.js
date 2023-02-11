d4ee86aad6a40ac70d9e472d96cf5c974174dfcd
Jonathn bay <bayart.jonathan@laposte.net>
2023-02-11 20:46:09 +0100

    trie des pays/continents + filter

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,6 +5,9 @@ import Card from './Card';
 const Countries = () => {
 
     const [data, setData] = useState([])
+    const [rangeValue, setRangeValue] = useState(36)
+    const [selectedRadio, setSelectedRadio] = useState("")
+    const radios = ["Africa", "America", "Asia", "Europe", "Oceania"]
 
     // Le useEffect se joue lorsque le composant est monté
     useEffect(() => {
@@ -13,10 +16,34 @@ const Countries = () => {
 
     return (
         <div className='countries'>
-            <h1>countries</h1>
+            <ul className='radio-container'>
+                <input type="range" 
+                min="1" 
+                max="250" 
+                defaultValue={rangeValue} 
+                onChange={(e) => setRangeValue(e.target.valueAsNumber)} />
+
+                {radios.map((continent) => (
+                    <li>
+                        <input type="radio" 
+                        id={continent} 
+                        name="continentRadio" 
+                        checked={continent === selectedRadio}
+                        onChange={(e) => setSelectedRadio(e.target.id)}/>
+                        <label htmlFor={continent}>{continent}</label>
+                    </li>
+                ))}
+            </ul>
+            {selectedRadio && (
+                <button onClick={() => setSelectedRadio("")}>Annuler la recherche</button>
+            )}
             <ul>
                 {
-                    data.map((country, index) => (
+                    data
+                    .filter((country) => country.continents[0].includes(selectedRadio))
+                    .sort((a, b) =>b.population -a.population)
+                    .slice(0, rangeValue)
+                    .map((country, index) => (
                        <Card key={index} country={country} /> 
                         ))
                 }
